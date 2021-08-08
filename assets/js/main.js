0891e84dda3fc2d145e9350d30477e04e6ec5d42
jmk008 <jmkrbhatt@gmail.com>
2021-08-08 21:02:44 +0530

    Basic OPL to Dev

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,6 +57,33 @@ $(function () {
         $(".navbar-toggler").removeClass('active');
     });    
     
+	//===== Isotope Project 1 == From Consult website
+    
+    $('.container').imagesLoaded(function () {
+        var $grid = $('.grid').isotope({
+        // options
+            transitionDuration: '1s',
+            itemSelector: '.grid-item',
+            percentPosition: true,
+            masonry: {
+                
+            }
+            
+        });
+        
+        // filter items on button click
+        $('.project-menu ul').on( 'click', 'li', function() {
+          var filterValue = $(this).attr('data-filter');
+          $grid.isotope({ filter: filterValue });
+        });
+        
+        //for menu active class
+        $('.project-menu ul li').on('click', function (event) {
+            $(this).siblings('.active').removeClass('active');
+            $(this).addClass('active');
+            event.preventDefault();
+        });
+    });
 
     //====== Magnific Popup
 
